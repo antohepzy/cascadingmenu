582c3044a814f4627defcc02a70e75c656f66b78
antohepzy <47741661+antohepzy@users.noreply.github.com>
2019-06-19 09:26:08 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import data from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+  }  
+  
+
+  renderSubMenu(menuOptions){
+        // loop through the menu items 
+        const subMenu = Object.values(menuOptions).map((option,i) => {
+
+        
+            return(
+              
+            <div> 
+              {/* check if item has subitems */}
+              {  (option.children.length > 0 )
+
+              ?
+                <div  className="subList">
+                <svg className="arrow" xmlns="http://www.w3.org/2000/svg" width="20" height="30" viewBox="0 0 14 14"><path d="M5 8l4 4 4-4z"/></svg> <input type="checkbox" id={option.item} />  <label htmlFor={option.item}>{option.item}</label>
+                {/* recursive call to loop through the subitems */}
+                <ul> {this.renderSubMenu(option.children)} </ul>
+                </div>   
+                
+                  
+              :
+              //display the individual items 
+              <li key={option.item} value= {option.item}><a href="#"> &#9776; &nbsp; {option.item}</a></li>
+
+              }
+             
+            </div>);
+            
+          }
+        );
+
+        return subMenu;
+      
+    }
+
+ 
+   render(){  
+    return (
+      //nav bar
+      <div>
+      <div className="nav">
+      </div>
+
+      {/*menu area*/}
+      <div className="menu"> 
+      <h4> MENU</h4>
+      <ul className = "main"> { this.renderSubMenu(data) } </ul>
+      </div>
+     
+      {/*content area*/}
+      <div className="content"> </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
